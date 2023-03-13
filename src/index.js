955a1c74f3f93ac481c2cdda52c209fdec6abd94
AbrarShahriar <AbrarShahriar321@gmail.com>
2023-03-13 20:38:25 +0600

    added default msg

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let matrixState = [];
+let matrixState = [0, 68, 64, 68, 116, 84, 84, 0];
 
 app.get("/", (req, res) => {
   res.status(200).json(matrixState);
