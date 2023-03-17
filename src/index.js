6f025f8e86dbb1ed85afa963e4bd093ceaafc01b
AbrarShahriar <AbrarShahriar321@gmail.com>
2023-03-17 16:25:36 +0600

    added fs

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 
 const PORT = process.env.PORT || 4001;
+const PATH = path.resolve(__dirname, "data.json");
+
+console.log(PATH);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-let matrixState = [0, 68, 64, 68, 116, 84, 84, 0];
-
 app.get("/", (req, res) => {
+  let RAW_matrixState = fs.readFileSync(PATH, { encoding: "utf-8" });
+  let matrixState = JSON.parse(RAW_matrixState);
   res.status(200).json(matrixState);
 });
 
 app.post("/", (req, res) => {
-  matrixState = req.body.matrixState;
-  res.status(200).json(matrixState);
+  let RES_matrixState = req.body.matrixState;
+
+  fs.writeFileSync(PATH, JSON.stringify(RES_matrixState, null, 2), {
+    encoding: "utf-8",
+  });
+
+  res.status(200).json(RES_matrixState);
 });
 
 app.listen(PORT, () => {
